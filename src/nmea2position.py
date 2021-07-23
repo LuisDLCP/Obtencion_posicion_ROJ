74dbfb49ad8d3d646db085996cc536a94c027c74
luis <luis11.dlcp@gmail.com>
2021-07-23 12:32:25 -0500

    some changes

--- src/nmea2position.py
+++ src/nmea2position.py
@@ -1,9 +1,12 @@
+#!/home/luis/anaconda3/bin/python3
 import pandas as pd
 import datetime
 import glob
+import os 
 
 current_path = "/home/luis/Desktop/Proyects_Files/LISN/GPSs/Tareas/Obtencion_posicion/"
-input_files_path = current_path + "data_input/"
+input_files_path = current_path + "data_input/data_set/"
+input_files_path_op = current_path + "data_input/data_procesada/"
 output_files_path = current_path + "data_output/"
 
 # Input file: nmea type GGA & ZDA file. 
@@ -15,35 +18,73 @@ def read_nmea(input_file_name):
 
 # Input file: pandas dataframe
 def nmea2lisn(data):
-    # Select gga data
+    # Select and clean GGA data: Time + Coordinates 
     data_gga = data.loc["$GPGGA"] 
     data_gga2 = data_gga[[1,2,3,4,5,9]] # Choose only the valid information
     data_gga2.columns = ["UTC_time", "LAT", "LAT(unit)", "LON", "LON(unit)", "HEIGHT"]
-    # Select zda data
+    data_gga2.reset_index(drop=True, inplace=True)
+    #
+    # -- Remove first columns, which have wrong data
+    vals = data_gga2.iloc[:10,0].values # extract first 10 rows 
+    indice = [v<=min(vals) for v in vals].index(True)
+    rm_rows = list(range(indice))
+    data_gga2.drop(index=rm_rows, inplace=True)
+    data_gga2.reset_index(drop=True, inplace=True)
+    #
+    # -- Remove last columns, wich have wrong data
+    vals = data_gga2.iloc[-10:,0].values # extract 10 last rows 
+    indice = [vv>=max(vals) for vv in vals].index(True)
+    a = list(range(len(vals)-indice-1))
+    rm_rows = [len(data_gga2)-(val+1) for val in a]
+    data_gga2.drop(index=rm_rows, inplace=True)
+    #
+    data_gga2.drop_duplicates(subset="UTC_time", keep='last', inplace=True)
+
+    # Select and clean ZDA data: Date
     data_zda = data.loc["$GPZDA"] 
     data_zda2 = data_zda[[1,2,3,4]]
     data_zda2.columns = ["UTC_time","day","month","year"]
+    data_zda2.reset_index(drop=True, inplace=True)
+    #
+    # --Remove the first columns, with wrong data
+    vals = data_zda2.iloc[:10,0].values
+    indice = [vv<=min(vals) for vv in vals].index(True)
+    rm_rows = list(range(indice))
+    data_zda2.drop(index=rm_rows, inplace=True)
+    data_zda2.reset_index(drop=True, inplace=True)
+    #
+    # --Remove the last columns, which have wrong data
+    vals = data_zda2.iloc[-10:,0].values
+    indice = [vv>=max(vals) for vv in vals].index(True)
+    a = list(range(len(vals)-indice-1))
+    rm_rows = [len(data_zda2)-(val+1) for val in a]
+    data_zda2.drop(index=rm_rows, inplace=True)
+    #
+    data_zda2.drop_duplicates(subset="UTC_time", keep="last", inplace=True)
 
-    # First, we obtain time info
-    data_gga2.reset_index(drop=True, inplace = True) # Drop label index
-    data_zda2.reset_index(drop=True, inplace = True)
+    # DATA PROCESSING 
+    # --Obtain location info 
+    def convert2_decimalDegree(value): # convert degree + decimal minute value to decimal degree
+        integer = int(value/100)
+        decimal = (value-integer*100)/60
+        return integer + decimal
 
-    def get_secondsDay(tiempo): # Get seconds of day from hour:minute:seconds, tiempo:"str"
-        hour = int(tiempo[:2])
-        minute = int(tiempo[2:4])
-        seconds = int(tiempo[4:])
-        #
-        total_seconds = seconds + minute*60 + hour*60*60
-        #
-        return total_seconds
-    
-    data_gga2["SoD"] = data_zda2["UTC_time"].astype("int").astype("str").apply(get_secondsDay)
+    def coordinate_sign(coord): # Change N,S,W,E to numbers
+        if coord == "S" or coord == "W":
+            return -1
+        elif coord == "N" or coord == "E":
+            return +1
+        else:
+            return "Error!"
 
-    colum_names = list(data_zda2.columns)[1:]
-    for i in colum_names:
-        data_zda2[i] = data_zda2[i].astype("int").astype("str") # Convert "day","month" and "year" to str variables
+    data_gga2.loc[:,"LAT"] = data_gga2.loc[:,"LAT"].apply(convert2_decimalDegree).round(6) * data_gga2.loc[:,"LAT(unit)"].apply(coordinate_sign)
+    del data_gga2["LAT(unit)"] # drop this column
 
-    def change_date(row): # Get YY/DOY from YYYY/MM/DD
+    data_gga2.loc[:,"LON"] = data_gga2.loc[:,"LON"].apply(convert2_decimalDegree).round(5) * data_gga2.loc[:,"LON(unit)"].apply(coordinate_sign)
+    del data_gga2["LON(unit)"] 
+
+    # --Get YY/DOY from YYYY/MM/DD
+    def change_date(row): 
         day = row[1]
         month = row[2]
         year = row[3]
@@ -56,36 +97,36 @@ def nmea2lisn(data):
         DOY = date2[3:] 
 
         return YEAR, DOY
-    
-    data_gga2["Year"] = data_zda2.apply(change_date, axis=1).str.get(0).astype("int") # Get the year YY
-    data_gga2["DoY"] = data_zda2.apply(change_date, axis=1).str.get(1).astype("int") # Get the day of year DOY
 
-    # Then, we obtain location info 
-    def convert2_decimalDegree(value): # convert degree + decimal minute value to decimal degree
-        integer = int(value/100)
-        decimal = (value-integer*100)/60
-        
-        return integer + decimal
-    
-    def coordinate_sign(coord): # Change N,S,W,E to numbers
-        if coord == "S" or coord == "W":
-            return -1
-        elif coord == "N" or coord == "E":
-            return +1
-        else:
-            return "Error!"
-    
-    data_gga2["LAT"] = data_gga2["LAT"].apply(convert2_decimalDegree).round(6) * data_gga2["LAT(unit)"].apply(coordinate_sign)
-    del data_gga2["LAT(unit)"] # drop this column
+    data_zda2["Year"] = data_zda2.astype('int').astype('str').apply(change_date, axis=1).str.get(0).astype("int") # Get the year YY
+    data_zda2["DoY"] = data_zda2.astype('int').astype('str').apply(change_date, axis=1).str.get(1).astype("int") # Get the day of year DOY
 
-    data_gga2["LON"] = data_gga2["LON"].apply(convert2_decimalDegree).round(5) * data_gga2["LON(unit)"].apply(coordinate_sign)
-    del data_gga2["LON(unit)"] 
+    del data_zda2["day"]
+    del data_zda2["month"]
+    del data_zda2["year"]
 
-    # Finally, we arrange the columns
-    data_gga2["Var"] = 0
-    data_gga2 = data_gga2[["Var","Year","DoY","SoD","LAT","LON","HEIGHT"]]
+    # --Join dfs 
+    df = data_gga2.merge(data_zda2, how='left', on='UTC_time')
 
-    return data_gga2
+    # --Get seconds of day(SOD) from hour:minute:seconds, tiempo:"str"
+    def get_secondsDay(tiempo): 
+        hour = int(tiempo[:2])
+        minute = int(tiempo[2:4])
+        seconds = int(tiempo[4:])
+        #
+        total_seconds = seconds + minute*60 + hour*60*60
+        #
+        return total_seconds
+
+    df["SoD"] = df.loc[:,"UTC_time"].astype('int').astype('str').str.zfill(6).apply(get_secondsDay)
+    del df["UTC_time"] 
+
+    # --Finally, we arrange the columns
+    df["Var"] = 0
+    df = df[["Var","Year","DoY","SoD","LAT","LON","HEIGHT"]]
+    df.drop_duplicates(subset="SoD", keep="last", inplace=True)
+
+    return df
 
 def save_csv(input_file_name, variable):
     output_file_name = input_file_name[:-4]+".pos"
@@ -96,12 +137,17 @@ def save_csv(input_file_name, variable):
 def main():
     list_input_files = glob.glob(input_files_path+'*.201') # List all the files of the input directory
     if len(list_input_files)>0:
-        for i in range(len(list_input_files)):
-            file_name = list_input_files[i][len(input_files_path):] # Get the file's name
+        for file_i in list_input_files:
+            file_name = file_i[len(input_files_path):] # Get the file's name
 
             dframe_nmea = read_nmea(file_name)
             dframe_lisn = nmea2lisn(dframe_nmea)
             save_csv(file_name, dframe_lisn)
+    
+            # Move input files to a permanent directory
+            os.rename(file_i, input_files_path_op+file_name)
 
 if __name__ == '__main__':
+    print("Starting ...")
     main()
+    print("Finished!")
